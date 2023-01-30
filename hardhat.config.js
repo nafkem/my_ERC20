e14f0a711e8b31597b861495133fcead48f848c7
Nofisat Abiodun Ayanlola <50343783+nafkem@users.noreply.github.com>
2023-01-30 17:07:51 +0100

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require("@nomiclabs/hardhat-waffle");
+require('@openzeppelin/hardhat-upgrades');
+// Any file that has require('dotenv').config() statement 
+// will automatically load any variables in the root's .env file.
+require('dotenv').config();
+ 
+module.exports = {
+  solidity: "0.8.17",
+  networks:{
+    goerli:{
+      // Ankr's Public RPC URL
+      url: "https://rpc.ankr.com/eth_goerli",
+     // PRIVATE_KEY loaded from .env file
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
+};
